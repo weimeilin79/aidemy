4f5848d4106848cf7bc78f537753197a6f6081cf
Christina Lin <weimeilin@gmail.com>
2025-02-24 23:07:32 -0500

    Update aidemy.py

--- planner/aidemy.py
+++ planner/aidemy.py
@@ -2,6 +2,7 @@ import os
 import random
 import requests
 import vertexai
+import json
 from typing import TypedDict, Literal
 from vertexai.preview import reasoning_engines
 from langchain_google_vertexai import ChatVertexAI
@@ -17,31 +18,48 @@ from langgraph.prebuilt import tools_condition
 from curriculums import get_curriculum 
 from search import search_latest_resource 
 from book import recommend_book 
+from onramp_workaround import get_next_region
+
+from google.cloud import pubsub_v1
 
 
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
 
+def send_plan_event(teaching_plan:str):
+    """
+    Send the teaching event to the topic called plan
+    
+    Args:
+        teaching_plan: teaching plan
+    """
+    publisher = pubsub_v1.PublisherClient()
+    print(f"-------------> Sending event to topic plan: {teaching_plan}")
+    topic_path = publisher.topic_path(PROJECT_ID, "plan")
 
-project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
+    message_data = {"teaching_plan": teaching_plan} 
+    data = json.dumps(message_data).encode("utf-8") 
 
-# Connect to resourse needed from Google Cloud
-llm = ChatVertexAI(model_name="gemini-2.0-flash-001")
+    future = publisher.publish(topic_path, data)
 
+    return f"Published message ID: {future.result()}"
 
-
-tools = [get_curriculum, search_latest_resource, recommend_book]
+tools = [get_curriculum, search_latest_resource, recommend_book,send_plan_event]
 
 def determine_tool(state: MessagesState):
-
+    llm = ChatVertexAI(model_name="gemini-2.0-flash-001", location=get_next_region())
     sys_msg = SystemMessage(
                     content=(
-                        "You are a helpful teaching assistant that help gathering all needed informations"
+                        f"""You are a helpful teaching assistant that helps gather all needed information. 
+                            Your ultimate goal is to create a detailed 3-week teaching plan. 
+                            You have access to tools that help you gather information.  
+                            Based on the user request, decide which tool(s) are needed. 
+
+                        """
                     )
                 )
 
     llm_with_tools = llm.bind_tools(tools)
     return {"messages": llm_with_tools.invoke([sys_msg] + state["messages"])} 
-
-
 
 def prep_class(prep_needs):
    
@@ -65,12 +83,4 @@ def prep_class(prep_needs):
     teaching_plan_result = messages["messages"][-1].content  
 
 
-    return teaching_plan_result# Return both
-
-
-
-    
-
-
-
-#prep_class("I'm doing a course for  year 5 on subject Mathematics in Geometry, , get school curriculum , and come up with few books recommendation plus  search latest resources on the internet base on the curriculum outcome. And come up with a 3 week teaching plan)")
+    return teaching_plan_result
