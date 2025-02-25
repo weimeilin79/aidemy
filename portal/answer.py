3a74fb974a762ac86c7a29c8ba19208de11c7a5f
Christina Lin <weimeilin@gmail.com>
2025-02-24 23:09:56 -0500

    Update answer.py

--- portal/answer.py
+++ portal/answer.py
@@ -1,25 +1,22 @@
 import json
 import os
+import time
 from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
 
 
-llm = VertexAI(model_name="gemini-2.0-flash-thinking-exp-01-21")
-#llm = VertexAI(model_name="gemini-2.0-flash-001")
-
-def answer_thinking(question, options, correct_answer):
+def answer_thinking(question, options, user_response, answer, region):
     try:
+        llm = VertexAI(model_name="gemini-2.0-flash-thinking-exp-01-21",location=region)
         
-       
-        input_msg = HumanMessage(content=[f"Here the question{question}, here are the avalible options {options}, this was the correct answer {correct_answer}"])
+        input_msg = HumanMessage(content=[f"Here the question{question}, here are the available options {options}, this student's answer {user_response}, whereas the correct answer is {answer}"])
         prompt_template = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(
                     content=(
-                        "You are a helpful teacher trying to teach the student on question, you were given the question and a set of mutiple choices "
-                        "Explain in detail how to come up with the answer, and why does each options are not correct."
-                        
+                        "You are a helpful teacher trying to teach the student on question, you were given the question and a set of multiple choices "
+                        "what's the correct answer. use friendly tone"
                     )
                 ),
                 input_msg,
@@ -35,3 +32,13 @@ def answer_thinking(question, options, correct_answer):
     except Exception as e:
         print(f"Error sending message to chatbot: {e}") # Log this error too!
         return f"Unable to process your request at this time. Due to the following reason: {str(e)}"
+
+
+
+if __name__ == "__main__":
+    question = "Evaluate the limit: lim (x→0) [(sin(5x) - 5x) / x^3]"
+    options = ["A) -125/6", "B) -5/3 ", "C) -25/3", "D) -5/6"]
+    user_response = "B"
+    answer = "A"
+    region = "us-central1"
+    result = answer_thinking(question, options, user_response, answer, region)
